5d8ebe59f58e7004b39b3baf9a0168b694a0de09
chenximo <37072197+chenximo@users.noreply.github.com>
2020-11-02 14:28:27 +0800

    fix: 捕获  createIM 错误
    Co-authored-by: lindalli <lindalli@tencent.com>

--- src/tim.js
+++ src/tim.js
@@ -1,13 +1,20 @@
 import TWebLive from 'tweblive'
-import { SDKAPPID } from '../public/debug/GenerateTestUserSig'
 const SDKAppID =  window.genTestUserSig('').SDKAppID
-
+import store from './store/index'
+let im = null
 // 初始化 SDK 实例
-const im = TWebLive.createIM({ SDKAppID })
 
-window.setLogLevel = im.setLogLevel
+try {
+  im = TWebLive.createIM({ SDKAppID })
+  window.setLogLevel = im.setLogLevel
 
 // 无日志级别
-im.setLogLevel(0)
+  im.setLogLevel(0)
+} catch (e) {
+  console.log(e)
+  store.commit('showMessage', { message: e, type: 'error' })
+}
+
+
 
 export default im
